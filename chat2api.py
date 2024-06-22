358ac8cb4f270d4c8f511c66f4d2b8e3a24c84ad
happylwws <49773818+happylwws@users.noreply.github.com>
2024-06-22 21:54:02 +0800

    Update chat2api.py

--- chat2api.py
+++ chat2api.py
@@ -4,6 +4,7 @@ import warnings
 import json
 from contextlib import asynccontextmanager
 
+
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from fastapi import FastAPI, Request, Depends, HTTPException, Form
 from fastapi.middleware.cors import CORSMiddleware
@@ -104,6 +105,7 @@ async def upload_post(text: str = Form(...)):
     for line in lines:
         if line.strip() and not line.startswith("#"):
             globals.token_list.append(line.strip())
+    #await write_token_file(globals.TOKENS_FILE, globals.token_list)
     logger.info(f"Token count: {len(globals.token_list)}, Error token count: {len(globals.error_token_list)}")
     tokens_count = len(globals.token_list)
     return {"status": "success", "tokens_count": tokens_count}
