34ed14f4b78214e0a8c6ede6210bda872aec16f2
happylwws <49773818+happylwws@users.noreply.github.com>
2024-06-22 16:15:43 +0800

    Update refreshToken.py

--- chatgpt/refreshToken.py
+++ chatgpt/refreshToken.py
@@ -10,9 +10,12 @@ from utils.config import proxy_url_list
 import chatgpt.globals as globals
 
 
-def save_refresh_map(refresh_map):
-    with open(globals.REFRESH_MAP_FILE, "w") as file:
-        json.dump(refresh_map, file)
+async def save_refresh_map(refresh_map):
+    try:
+        content = json.dumps(refresh_map).encode()
+        globals.dbx.files_upload(content, globals.REFRESH_MAP_FILE, mode=globals.WriteMode('overwrite'))
+    except Exception as e:
+        raise e
 
 
 async def rt2ac(refresh_token, force_refresh=False):
@@ -24,7 +27,7 @@ async def rt2ac(refresh_token, force_refresh=False):
         try:
             access_token = await chat_refresh(refresh_token)
             globals.refresh_map[refresh_token] = {"token": access_token, "timestamp": int(time.time())}
-            save_refresh_map(globals.refresh_map)
+            await save_refresh_map(globals.refresh_map)
             logger.info(f"refresh_token -> access_token with openai: {access_token}")
             return access_token
         except HTTPException as e:
@@ -38,21 +41,30 @@ async def chat_refresh(refresh_token):
         "redirect_uri": "com.openai.chat://auth0.openai.com/ios/com.openai.chat/callback",
         "refresh_token": refresh_token
     }
+    oai_data={"refresh_token": refresh_token}
     client = Client(proxy=random.choice(proxy_url_list) if proxy_url_list else None)
     try:
-        r = await client.post("https://auth0.openai.com/oauth/token", json=data, timeout=5)
+        #r = await client.post("https://auth0.openai.com/oauth/token", json=data, timeout=5)
+        r = await client.post("https://token.oaifree.com/api/auth/refresh", data=oai_data, timeout=5)
         if r.status_code == 200:
             access_token = r.json()['access_token']
             return access_token
         else:
-            with open(globals.ERROR_TOKENS_FILE, "a", encoding="utf-8") as f:
-                f.write(refresh_token + "\n")
-            if refresh_token not in globals.error_token_list:
-                globals.error_token_list.append(refresh_token)
+            if "access_denied" in r.text or "invalid_grant" in r.text:
+                await globals.del_token(refresh_token)
+                globals.count -= 1
             raise Exception(r.text[:100])
     except Exception as e:
-        logger.error(f"Failed to refresh access_token `{refresh_token}`: {str(e)}")
-        raise HTTPException(status_code=500, detail=f"Failed to refresh access_token.")
+        raise HTTPException(status_code=500, detail=str(e))
     finally:
         await client.close()
         del client
+
+
+async def ac2rt(access_token):
+    "从refresh_map里找access_token对应的refresh_token"
+    '如果没找到,返回None,说明该access_token是用户上传的,非由refresh_token转化而来'
+    for k, v in globals.refresh_map.items():
+        if v["token"] == access_token:
+            return k
+    return None
